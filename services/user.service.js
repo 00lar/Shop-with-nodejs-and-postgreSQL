480a7dfa572dbfe6f7ba603298829d0fa72db9c7
00lar <eduardani84@gmail.com>
2023-12-01 13:11:31 -0700

    Crud database

--- services/user.service.js
+++ services/user.service.js
@@ -1,32 +1,39 @@
 const boom = require('@hapi/boom');
-const getConnection = require("../lib/postgres")
+
+const {models} = require("../lib/sequelize");
+const { use } = require('../routes/users.router');
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data)
+    return newUser
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query("SELECT * FROM task")
-    return rta.rows;
+    const rta = await models.User.findAll()
+    return rta
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id)
+    if (!user) {
+      throw boom.notFound("user not found")
+    }
+    return user
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id)
+    const rta = await user.update(changes)
+    return rta
   }
 
   async delete(id) {
-    return { id };
+    const user = await models.User.findByPk(id)
+    await user.destroy()
+    return {id}
   }
 }
 
